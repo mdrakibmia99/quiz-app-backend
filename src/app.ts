fc04d4b134d1e5361edd1d29ddbf1bd285686b6d
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-13 19:45:44 +0600

    add new cookieParser

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import cors from 'cors';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import router from './app/routes';
 import { StatusCodes } from 'http-status-codes';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
 // parser
 
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application router
 // all route
