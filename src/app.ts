73b4ab1e717905be75e686a56c8efbf613de362c
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-14 22:27:38 +0600

    modify app file

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173'],credentials: true }));
 
 // application router
 // all route
