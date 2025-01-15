2af64af180f6577868122b8ea246e5d7d5f353f7
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-15 21:34:02 +0600

    for quiz result create validation

---
+++ src/app/modules/resultInfo/quizResult.validation.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const quizResultValidationSchema = z.object({
+  body: z.object({
+    userId: z.string({required_error:"userId required"}),
+    quizId: z.string({required_error:"quizId required"}),
+    userAnswer: z.array(z.string()), // Array of strings
+    moreResultInfo: z.boolean(),
+    quizComplete: z.boolean(),
+  }),
+});
+
+export const quizResultValidation = {
+   quizResultValidationSchema,
+};
