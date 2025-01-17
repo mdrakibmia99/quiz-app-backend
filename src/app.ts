08ae3f1356a99f463e2ae93f59cf79bb44bef850
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-17 21:33:44 +0600

    update app

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'],credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173','https://quiz-app-rkrakib.vercel.app'],credentials: true }));
 
 // application router
 // all route
